4b3000ca7bc32ed39676628f8d02dbb10bc0e7ac
Andy Polyakov <appro@cryptogams.org>
2021-07-30 15:50:32 +0200

    src/sloth256_189.cu: add test_kernel.

--- src/sloth256_189.cu
+++ src/sloth256_189.cu
@@ -1,5 +1,30 @@
 #include <cuda.h>
 
+#include "sloth256_189.c"
+
+__global__ void test_kernel(unsigned char *piece_n_iv, size_t len,
+                            size_t layers)
+{
+    (void)sloth256_189_encode(piece_n_iv, len, piece_n_iv+len, layers);
+}
+
+extern "C"
+void sloth256_189_encode_cuda(unsigned char piece[4096], size_t len,
+                              const unsigned char iv[32], size_t layers)
+{
+    unsigned char *piece_n_iv;
+
+    cudaMalloc(&piece_n_iv, len+32);
+    cudaMemcpy(piece_n_iv, piece, len, cudaMemcpyHostToDevice);
+    cudaMemcpy(piece_n_iv+len, iv, 32, cudaMemcpyHostToDevice);
+
+    test_kernel<<<1, 1>>>(piece_n_iv, len, layers);
+
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(piece, piece_n_iv, len, cudaMemcpyDeviceToHost);
+}
+
 extern "C" bool detect_cuda()
 {
     cudaDeviceProp prop;
