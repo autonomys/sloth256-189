aef190c7b26113bd38b2307ec729976d373017ef
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2022-05-17 14:54:26 +0300

    Fix `no_std` support

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
-#![warn(rust_2018_idioms, missing_debug_implementations, missing_docs)]
 //! A Rust wrapper around both C/assembly and CUDA implementation of Sloth permutation suggested in
 //! <https://eprint.iacr.org/2015/366>, extended for a proof-of-replication, and instantiated for
 //! 2**256-189 modulus used in Subspace Network.
 //!
 //! Universal CPU implementation is always available, for CUDA support make sure to enable `cuda`
 //! feature and have CUDA toolchain installed.
+
+#![warn(rust_2018_idioms, missing_debug_implementations, missing_docs)]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod cpu;
 #[cfg(feature = "cuda")]
